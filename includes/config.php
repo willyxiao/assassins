90e314d4089aee0304b5246d379f47a4d650803a
WilliamXiao <willy@chenxiao.us>
2014-04-25 12:03:32 -0700

    New game configured

--- includes/config.php
+++ includes/config.php
@@ -3,4 +3,6 @@
 require("constants.php"); 
 require("functions.php"); 
 
+ini_set("log_errors", 1); 
+ini_set("error_log", "../data/error_log"); 
 ?>
---
+++ public/new_game.php
@@ -0,0 +1,71 @@
+<?php
+function get_randomword($len = 10) {
+    $word = array_merge(range('a', 'z'), range('A', 'Z'));
+    shuffle($word);
+    return substr(implode($word), 0, $len);
+}
+
+
+	require("../includes/config.php"); 
+	
+	if(!NEW_GAME_RECEIVING) {
+		die("NOPEFISH"); 
+	}	
+
+	if($_SERVER["REQUEST_METHOD"] == "GET") {
+		render("new_game_template.php"); 
+	} else {
+		echo "<p>" ;
+		var_dump($_FILES); 		
+		echo "</p>"; 		
+
+		$uploaddir = "../data/"; 
+		$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+		
+		var_dump($uploadfile); 
+		if(move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+			echo "<p>Uploaded file...working on moving into MySQL Table</p>"; 
+		} else {
+			die("Possible file upload attack!"); 
+		}	
+			
+		$csvData = file_get_contents($uploadfile);
+		$lines = explode(PHP_EOL, $csvData);
+		$array = array();
+		foreach ($lines as $line) {
+			$array[] = str_getcsv($line);
+		}
+		
+		echo "<p>"; 
+		var_dump($array); 
+		echo "</p>"; 
+		
+		for($i = 0; $i < 7; $i++) {
+			shuffle($array); 
+		}
+		
+		query("TRUNCATE TABLE users"); 
+		query("TRUNCATE TABLE killstory"); 
+		
+		for($i = 0: $i < count($array); $i++) {
+			$pw = get_randomword(5); 
+			$to = $array[$i][1] . "@college.harvard.edu"; 
+			$subject = "Your mission, should you choose to accept it..."; 
+			$message = 
+				"Welcome to Eliot House Assassins. Your missions is a dangerous one and not to be taken lightly. To find your target, visit: " . URL . ". \n"
+				. "Your username is your @college username and your password is: " . $pw . "\n"
+				. "Do not show anyone your password and do not give anyone the link once you login...security is not that tight. Hence why it is dangerous...\n"
+				. "May the odds be ever in your favor,\n"
+				. "FDDE"; 
+			$from = "From: willy@williamxiao.com\r\n";
+			mail($to, $subject, $message, $from);  
+			query("INSERT INTO users (userid, name, uname, password, dead, killed, to_kill) VALUES (?,?,?,?,?,?)", 
+				$i + 1, $array[$i][0], $array[$i][1], $pw, 0, 0, ($i == count($array) - 1 ? 1 : $i + 2));     	
+		}
+		
+		echo "<p>Done! May the games begin.</p>"; 
+	}
+
+	
+
+?>
