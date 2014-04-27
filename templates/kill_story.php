8ee5237910e38050355b1c2d5ab04721bd865ca3
WilliamXiao <willy@chenxiao.us>
2014-04-27 11:11:47 -0700

    Kill stories updated

--- templates/kill_story.php
+++ templates/kill_story.php
@@ -1,33 +1,43 @@
+<?php 
+	$killstories = query("SELECT story, TIME(ADDTIME(time, '3:0:0.0')) as time  FROM killstory WHERE is_kill_story=1 ORDER BY time DESC"); 
+	$deathstories = query("SELECT story, TIME(ADDTIME(time, '3:0:0.0')) as time FROM killstory WHERE is_kill_story != 1 ORDER BY time DESC"); 
+?>
 <div class="row">
-	<?php
-		$stories = query("SELECT story, time FROM killstory ORDER BY time DESC LIMIT 3"); 
-		$killstory1 = $stories[0]["story"];
-		$killtime1 = $stories[0]["time"]; 
-		$killstory2 = $stories[1]["story"];
-		$killtime2 = $stories[1]["time"]; 
-		$killstory3 = $stories[2]["story"];
-		$killtime3 = $stories[3]["time"];   
-	?>
-
-	<div class="small-4 large-4 columns">
-		<h1>Kill story #1</h1>
-		<p>At <?= htmlspecialchars($killtime1) ?></p>
-		<p>
-		<?= htmlspecialchars($killstory1) ?>
-		</p>
+	<h1>Kill Stories</h1>
+	<div class="small-6 large-6 columns">
+	<table>
+	  <thead>
+	    <tr>
+	      <th>Time</th>
+	      <th width="400">Kill Story</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	  	<?php foreach($killstories as $killstory) :?>
+		<tr>
+			<td><?= htmlspecialchars($killstory["time"]) ?></td>
+			<td><?= htmlspecialchars($killstory["story"]) ?></td>
+		</tr>
+		<?php endforeach; ?>
+	  </tbody>
+	</table>
 	</div>
-	<div class="small-4 large-4 columns">
-		<h1>Kill story #2</h1>
-		<p>At <?= htmlspecialchars($killtime2) ?> </p>
-		<p>
-		<?= htmlspecialchars($killstory2) ?>
-		</p>
-	</div>
-	<div class="small-4 large-4 columns">
-		<h1>Kill story #3</h1>
-		<p>At <?= htmlspecialchars($killtime3) ?></p>
-		<p>
-		<?= htmlspecialchars($killstory3) ?>
-		</p>
+	<div class="small-6 large-6 columns">
+	<table>
+	  <thead>
+	    <tr>
+	      <th>Time</th>
+	      <th width="400">Death Story</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	  	<?php foreach($deathstories as $killstory) :?>
+		<tr>
+			<td><?= htmlspecialchars($killstory["time"]) ?></td>
+			<td><?= htmlspecialchars($killstory["story"]) ?></td>
+		</tr>
+		<?php endforeach; ?>
+	  </tbody>
+	</table>
 	</div>
 </div>
