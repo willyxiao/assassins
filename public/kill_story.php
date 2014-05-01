5532729eb576432ff7be6e47a9919e6d35e50984
WilliamXiao <willy@chenxiao.us>
2014-04-30 18:44:51 -0700

    kill_story_js_loader

---
+++ public/kill_story.php
@@ -0,0 +1,42 @@
+<?php
+	require("../includes/config.php"); 
+
+	$killstories = query("SELECT story, TIME(ADDTIME(time, '3:0:0.0')) as time2  FROM killstory WHERE is_kill_story=1 ORDER BY time DESC"); 
+	$deathstories = query("SELECT story, TIME(ADDTIME(time, '3:0:0.0')) as time2 FROM killstory WHERE is_kill_story != 1 ORDER BY time DESC"); 
+?>
+	<div class="small-6 large-6 columns">
+	<table>
+	  <thead>
+	    <tr>
+	      <th>Time</th>
+	      <th width="400">Kill Story</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	  	<?php foreach($killstories as $killstory) :?>
+		<tr>
+			<td><?= htmlspecialchars($killstory["time2"]) ?></td>
+			<td><?= htmlspecialchars(stripslashes($killstory["story"])) ?></td>
+		</tr>
+		<?php endforeach; ?>
+	  </tbody>
+	</table>
+	</div>
+	<div class="small-6 large-6 columns">
+	<table>
+	  <thead>
+	    <tr>
+	      <th>Time</th>
+	      <th width="400">Death Story</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	  	<?php foreach($deathstories as $killstory) :?>
+		<tr>
+			<td><?= htmlspecialchars($killstory["time2"]) ?></td>
+			<td><?= htmlspecialchars(stripslashes($killstory["story"])) ?></td>
+		</tr>
+		<?php endforeach; ?>
+	  </tbody>
+	</table>
+	</div>
