e88e723bcbadb86c4f685ed4ef76dfe5b9d28d96
Willy Xiao <wxiao@hcs.harvard.edu>
2015-04-23 23:10:24 -0400

    rules

--- templates/rules.php
+++ templates/rules.php
@@ -1,34 +1,36 @@
 
- <div id="frame">
-        
-<h1>Eliot Assassins Rules (it does!)</h1>
-<DL>
-<DT><STRONG>To Play:</STRONG>
-<DD>If you've signed up, you will recieve an email with login information
-<DT><STRONG>Winning:</STRONG>
-<DD>There are two ways to win the game. The first is to be the last surviving player, and the second is to be the player with the most number of kills at the end of the game. It is therefore your goal to kill as many players as possible, and stay alive as long as possible.
-<DT><STRONG>Game Play:</STRONG>
-<DD>Each player will be assigned a target at the start of the game, at 11:59PM on MONDAY April 28th. A player must eliminate his or her target by any of the allowed methods (see "The Kill") and report the hit on the website, as well as confirm your death if misfortune falls upon you. You will be given the name of your target, but not the name of the assassin out to get you. Upon reporting the assassination of your target on the website, and their confirmation of death, you will be assigned a new target. Also it is expected that you have at least one meal in Eliot per day.
-<DT><STRONG>Safe Zones:</STRONG>
-<DD>The kill cannot take place in the food serving area in the dining hall, but can take place in the dining hall during non-meal hours (brain break is NOT safe).  The kill cannot take place in bathrooms (that would be weird). The kill cannot take place in ANY library: Eliot Library, Lamont, Widener, etc. (we don't want elaborately crafted study guides or precious books to be destroyed). The kill cannot take place at any IM events. The kill cannot take place at Fete (we take Fete seriously), or other Eliot sanctioned events such as Stein Clubs, Hoco Meetings, the BBQ on May 1st, Swing Dance Lessons, Strawberry Dipping, etc. Email keskici@college.harvard.edu if you have any questions.. Kills do not have to take place in Eliot, but the kill cannot take place if the target is at work or in class). The kill cannot take place at The Inferno (the Grille) while it is open for business. If you are assigned to kill your suitemate, the kill cannot take place in your suite.
-<DT><STRONG>Stunning:</STRONG>
-<DD>To stun the assassin who is out to get you, score a kill against the assassin (see "The Kill").  A stunned assassins cannot try to kill you for  an hour.
-<DT><STRONG>The Kill:</STRONG>
-<DD>You may score a kill against your target by any of the following means:
-<DD>- Hitting them with a shot from a nerf gun
-<DD>- Hitting them with a shot from a water gun.
-<dd>- Hitting them with a direct impact from a water balloon. (Hitting the floor near them, and having them exposed to some of the nearby splash does not count.)
-NOTE: don't be mean with water. If your target is using a computer when you go to assassinate him or her, be careful. No one likes mean people. Eliot Assassins claims
-no responsibility for property damaged during the game - that is your responsibility, so don't be dumb.
-<dd>- Stabbing them with carboard sword (read: a piece of cardboard)
-<dd>- Marking your target's skin with a sharpie
-<dd>- Poisoning your target with tabasco sauce. A player is successfully poisoned if tabasco sauce is put in their food without their knowledge and when the target finishes the food, they discover a note near the scene of the meal informing them of the food poisoning.
-<dd>- Hitting them with a pillow
-<dd> Note: Pillows are special in that you may use one as a shield as well as a weapon. You may carry only one pillow
-<DT><STRONG>After The Kill:</STRONG>
-<DD>It is the killer's responsibility to report it to http://assassin.willyxiao.com/.  The victim will must also confirm their death ont he site. If the victim wishes to contest the death, as they believe they were not actually killed according to game rules, they should email keskici@college.harvard.edu . Note that saying you killed your target when this is not in fact the case is not looked favorably upon, and may result in repercussions. Ambiguous kills or "ties" go to the assassin, since they managed to get that far in the first place.
-<DT><STRONG>About These Rules:</STRONG>
-<DD>Check your email and this site periodically throughout the game. If there are any changes to these rules, you will be notified.
-</div> <!-- end of frame -->
+ <!-- <div id="frame"> -->
+<div class="row">
+  <div class="large-12 columns">
+    <h1>Eliot Assassins Rules (it does!)</h1>
+    <DL>
+    <DT><STRONG>To Play:</STRONG>
+    <DD>If you've signed up, you will recieve an email with login information
+    <DT><STRONG>Winning:</STRONG>
+    <DD>There are two ways to win the game. The first is to be the last surviving player, and the second is to be the player with the most number of kills at the end of the game. It is therefore your goal to kill as many players as possible, and stay alive as long as possible.
+    <DT><STRONG>Game Play:</STRONG>
+    <DD>Each player will be assigned a target at the start of the game, at 11:59PM on MONDAY April 28th. A player must eliminate his or her target by any of the allowed methods (see "The Kill") and report the hit on the website, as well as confirm your death if misfortune falls upon you. You will be given the name of your target, but not the name of the assassin out to get you. Upon reporting the assassination of your target on the website, and their confirmation of death, you will be assigned a new target. Also it is expected that you have at least one meal in Eliot per day.
+    <DT><STRONG>Safe Zones:</STRONG>
+    <DD>The kill cannot take place in the food serving area in the dining hall, but can take place in the dining hall during non-meal hours (brain break is NOT safe).  The kill cannot take place in bathrooms (that would be weird). The kill cannot take place in ANY library: Eliot Library, Lamont, Widener, etc. (we don't want elaborately crafted study guides or precious books to be destroyed). The kill cannot take place at any IM events. The kill cannot take place at Fete (we take Fete seriously), or other Eliot sanctioned events such as Stein Clubs, Hoco Meetings, the BBQ on May 1st, Swing Dance Lessons, Strawberry Dipping, etc. Email keskici@college.harvard.edu if you have any questions.. Kills do not have to take place in Eliot, but the kill cannot take place if the target is at work or in class). The kill cannot take place at The Inferno (the Grille) while it is open for business. If you are assigned to kill your suitemate, the kill cannot take place in your suite.
+    <DT><STRONG>Stunning:</STRONG>
+    <DD>To stun the assassin who is out to get you, score a kill against the assassin (see "The Kill").  A stunned assassins cannot try to kill you for  an hour.
+    <DT><STRONG>The Kill:</STRONG>
+    <DD>You may score a kill against your target by any of the following means:
+    <DD>- Hitting them with a shot from a nerf gun
+    <DD>- Hitting them with a shot from a water gun.
+    <dd>- Hitting them with a direct impact from a water balloon. (Hitting the floor near them, and having them exposed to some of the nearby splash does not count.)
+    NOTE: don't be mean with water. If your target is using a computer when you go to assassinate him or her, be careful. No one likes mean people. Eliot Assassins claims
+    no responsibility for property damaged during the game - that is your responsibility, so don't be dumb.
+    <dd>- Stabbing them with carboard sword (read: a piece of cardboard)
+    <dd>- Marking your target's skin with a sharpie
+    <dd>- Poisoning your target with tabasco sauce. A player is successfully poisoned if tabasco sauce is put in their food without their knowledge and when the target finishes the food, they discover a note near the scene of the meal informing them of the food poisoning.
+    <dd>- Hitting them with a pillow
+    <dd> Note: Pillows are special in that you may use one as a shield as well as a weapon. You may carry only one pillow
+    <DT><STRONG>After The Kill:</STRONG>
+    <DD>It is the killer's responsibility to report it to http://assassin.willyxiao.com/.  The victim will must also confirm their death ont he site. If the victim wishes to contest the death, as they believe they were not actually killed according to game rules, they should email keskici@college.harvard.edu . Note that saying you killed your target when this is not in fact the case is not looked favorably upon, and may result in repercussions. Ambiguous kills or "ties" go to the assassin, since they managed to get that far in the first place.
+    <DT><STRONG>About These Rules:</STRONG>
+    <DD>Check your email and this site periodically throughout the game. If there are any changes to these rules, you will be notified.
+  </div>
+</div>
 </body>
 </html>
