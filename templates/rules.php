616189e7494bc599c5fb4ba67d821a42fcf3193b
Willy Xiao <wxiao@hcs.harvard.edu>
2015-04-23 23:14:22 -0400

    rules fixed

--- templates/rules.php
+++ templates/rules.php
@@ -1,7 +1,7 @@
 
  <!-- <div id="frame"> -->
 <div class="row">
-  <div class="small-8 small-centered columns">
+  <div class="small-10 small-centered columns">
     <h1>Eliot Assassins Rules (it does!)</h1>
     <DL>
     <DT><STRONG>To Play:</STRONG>
