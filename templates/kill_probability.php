72f333170709923a8cbed8ae409446e6ffebb223
Willy Xiao <wxiao@hcs.harvard.edu>
2015-04-26 20:50:54 -0400

    expected times

--- templates/kill_probability.php
+++ templates/kill_probability.php
@@ -1,4 +1,4 @@
-<a href="https://medium.com/@willyxiao/a410d6d76a2d"><h3>Predicted Next Kill</h3></a>
+<h3><a href="https://medium.com/@willyxiao/a410d6d76a2d">Predicted Next Kill</a></h3><
 <table>
   <thead>
     <tr>
@@ -9,10 +9,23 @@
   <tbody id="probTBody">
   </tbody>
 </table>
+<h3>Expected Time Until</h3>
+<table>
+  <thead>
+    <tr>
+      <th>Players Left</th>
+      <th>Days</th>
+    </tr>
+  </thead>
+  <tbody id="eTBody">
+  </tbody>
+</table>
 <script>
   $(document).ready(function(){
-    var alpha = 50.001
-    var beta = 3896.786
+    var alive = <?= $alive ?>
+
+    var alpha = 79.001
+    var beta = 6176.9
 
     function adjust(hour){
       var timeSubtract = 0
@@ -35,13 +48,31 @@
     }
 
     function find_prob(hour){
-      var prob = (-1*Math.pow(beta,alpha))*Math.pow(beta+adjust(hour)*(<?= $alive ?>),-1*alpha) + 1;
+      var prob = (-1*Math.pow(beta,alpha))*Math.pow(beta+adjust(hour)*(alive),-1*alpha) + 1;
       return "<tr><td>" + String(hour) + "</td><td>" + String(prob).substring(0,5) + "</td></tr>";
     }
 
     var hours = [1,2,4,8,12,24]
     for(n in hours){
       $("#probTBody").append(find_prob(hours[n]))
+    }
+
+
+    var post_mean = beta/alpha
+    var expected_times = []
+    var total_expected_times = []
+    var total_sum = 0
+    for(n = alive; n > 0; n--){
+      total_sum += post_mean/n
+      expected_times[expected_times.length] = post_mean/n
+      total_expected_times[total_expected_times.length] = total_sum
+    }
+
+    var playersLeft = [30, 25, 20, 15, 10]
+    for(n in playersLeft){
+      if(playersLeft[n] < alive){
+        $("eTBody").append("<tr><td>" + playersLeft[n].toString() + "</td><td>" + (total_expected_times[(alive - playersLeft[n] - 1)]/16).toString() + "</td></tr>"
+      }
     }
   })
 </script>
